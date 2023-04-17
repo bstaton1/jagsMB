115047f5395ccb2e8f06b969fb35f2fc0164cfc6
Ben <statonbe@gmail.com>
2023-04-17 16:33:46 -0700

    Create model_lines() function

--- R/export-fns.R
+++ R/export-fns.R
@@ -184,3 +184,76 @@ model_read = function(model_file = jagsMB_opts("model_file"),
   return(code)
 }
 
+#' View Lines from a JAGS Model File
+#'
+#' Print specific lines from a JAGS model file
+#' to the console. Enables quick printing
+#' for Rmd explanations or viewing line numbers
+#' referenced in syntax errors return by JAGS.
+#'
+#' @inheritParams model_read
+#' @param lines Numeric vector specifying the line numbers to display.
+#' @param show_labels Logical flag indicating whether to display line
+#'   numbers with the output (defaults to `TRUE`).
+#' @param keep_first_last Logical flag indicating whether to display
+#'   the first and last line in the model code.
+#'   These lines purely show `"model {"` and `"} # END OF MODEL"`,
+#'   so sometimes it may be desirable to omit them.
+#' @export
+
+model_lines = function(model_file = jagsMB_opts("model_file"),
+                       lines = NULL,
+                       show_labels = TRUE,
+                       keep_first_last = TRUE
+                       ) {
+
+  # check the model_file argument for validity
+  check_model_file(model_file)
+
+  # read in model code
+  model_code = model_read(model_file)
+
+  # count the lines
+  n_lines = length(model_code)
+
+  # stop if requested lines are outside of the model
+  if (any(lines > n_lines)) {
+    stop ("the model only contains ", n_lines, " lines")
+  }
+
+  # get line numbers
+  line_nums = 1:n_lines
+  max_digits = max(nchar(line_nums))
+  line_nums = stringr::str_pad(line_nums, width = max_digits, pad = "0")
+  line_nums = paste0("L", line_nums, "| ")
+
+  # if lines == NULL, print all lines
+  if (is.null(lines)) {lines = 1:n_lines}
+
+  # drop if not keeping the first and last lines
+  if (!keep_first_last) {
+    drop = c(1, n_lines)
+    lines = lines[-drop] - 1
+    line_nums = line_nums[-drop]
+    model_code = model_code[-drop]
+  }
+
+  # extract the lines to print
+  show_code = model_code[lines]
+
+  # find the amount of whitespace to trim off the front of all lines
+  n_lws = show_code |>
+    stringr::str_extract("^\\s+") |>
+    stringr::str_count("\\s")
+  n_lws[is.na(n_lws)] = 0
+
+  # trim this whitespace off
+  show_code = drop_lws(show_code, n = min(n_lws))
+
+  # if labeling lines, paste on line labels
+  if (show_labels) show_code = paste0(line_nums[lines], show_code)
+
+  # print the output to the console
+  cat(show_code, sep = "\n")
+}
+
